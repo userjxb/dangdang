dbedcc3fca8fc5634957561b7a39c304c69ac15f
userjxb <1315109044@qq.com>
2019-05-07 10:20:08 +0800

    5.7上午

---
+++ src/script/index.js
@@ -0,0 +1,18 @@
+/* 主页js效果 */
+// 导入页头和页尾
+;(function ($) {
+    $(".header").load("header.html"); // 引入头部html
+    $(".footer").load("footer.html"); // 引入尾部html
+})(jQuery);
+
+//轮播图
+;(function ($) {
+    class Silder {
+        constructor() {
+            this.olBtns = $(".slider-tab .btns li"); //小圆点
+            this.pics = $(".slider-tab .pics li"); //图片
+        }
+
+        
+    }
+})(jQuery);
